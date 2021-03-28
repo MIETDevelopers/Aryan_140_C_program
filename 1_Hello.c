5514191f109d939a938bd6d2112807f14f09fdfc
aryankoul2003 <76394260+aryankoul2003@users.noreply.github.com>
2021-03-28 11:36:18 +0530

    hello world program

--- 1_Hello.c
+++ 1_Hello.c
@@ -1,6 +1,5 @@
-#include <stdio.h>
-int main() {
-   // printf() displays the string inside quotation
-   printf("Hello, World!");
-   return 0;
-}
+#include <stdio.h>      //Pre Prossive directive to include standard input  and output function header file.
+int main() {   //start of main function body.
+   printf("Hello, World!");   //print f function called to print"Hello, World!".
+   return 0;   //return statement.
+}  //end of main function body.

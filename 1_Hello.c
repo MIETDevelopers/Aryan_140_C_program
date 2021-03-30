3a8b4280a7fbfe7d10428dce36d12e1e65e3525f
aryankoul2003 <2020a1r140@miet.in>
2021-03-31 01:37:22 +0530

    hello world file

--- 1_Hello.c
+++ 1_Hello.c
@@ -1,5 +1,5 @@
-#include <stdio.h>      //Pre Prossive directive to include standard input  and output function header file.
-int main() {   //start of main function body.
-   printf("Hello, World!");   //print f function called to print"Hello, World!".
-   return 0;   //return statement.
-}  //end of main function body.
+#include<stdio.h>//header file
+int main() {
+	printf("hello world");//printing hello world 
+	return 0;
+}
